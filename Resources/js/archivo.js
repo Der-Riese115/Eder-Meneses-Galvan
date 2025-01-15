6f761c52613f17a5f5259ab694687e746654ec98
Eder Meneses Galvan <darktiger_115@hotmail.com>
2025-01-14 20:00:03 -0600

    Se genero una dispocición para el apartado de contacto con sus datos

--- Resources/js/archivo.js
+++ Resources/js/archivo.js
@@ -1,8 +1,10 @@
 alert("Bienvenido");
 function showContact() {
+    document.getElementById('contactBut').classList.add('show');
     document.getElementById('contactDiv').classList.add('show');
 }
 
 function hideContact() {
     document.getElementById('contactDiv').classList.remove('show');
+    document.getElementById('contactBut').classList.remove('show');
 }
