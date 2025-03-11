ad84a523a367503888af262af9de73f325b2608b
Eder Meneses <eder_isc_mg101203@outlook.com>
2025-03-10 20:09:44 -0600

    Finalización del entorno

--- Resources/js/archivo.js
+++ Resources/js/archivo.js
@@ -1,40 +1,32 @@
-alert("Bienvenido");
+document.addEventListener("DOMContentLoaded", function () {
+    alert("Bienvenido a la página de Eder Meneses Galvan");
+});
+
 function showContact() {
-    document.getElementById('contactBut').classList.add('show');
     document.getElementById('contactDiv').classList.add('show');
 }
 
 function hideContact() {
     document.getElementById('contactDiv').classList.remove('show');
-    document.getElementById('contactBut').classList.remove('show');
 }
 
 function copiarTexto(idBoton) {
-    // Define el mapeo de botones y sus respectivos campos de texto
     const botonToTexto = {
-        'btn_eml': 'd_eml',
-        'btn_tel': 'd_tel',
-        'btn_dir': 'd_dir',
-        'btn_cop': 'd_cop'
+        'btn_eml': 'eder_isc_mg101203@outlook.com',
+        'btn_tel': '5544468317',
+        'btn_dir': 'Teoloyucan, Estado de México',
+        'btn_cop': '54786'
     };
 
-    // Busca el ID asociado al botón presionado
-    const idTexto = botonToTexto[idBoton];
-    if (!idTexto) {
-        console.error('No se encontró un campo de texto para el botón con ID:', idBoton);
+    const texto = botonToTexto[idBoton];
+    if (!texto) {
+        console.error('No se encontró un texto asociado al botón con ID:', idBoton);
         return;
     }
 
-    // Obtiene el valor del texto del elemento con ese ID
-    const elementoTexto = document.getElementById(idTexto);
-    if (!elementoTexto) {
-        console.error('No se encontró el elemento de texto con ID:', idTexto);
-        return;
-    }
-
-    navigator.clipboard.writeText(elementoTexto.value)
+    navigator.clipboard.writeText(texto)
         .then(() => {
-            alert('Dato copiado al portapapeles: ' + elementoTexto.value);
+            alert('Dato copiado al portapapeles: ' + texto);
         })
         .catch(err => {
             console.error('Error al copiar el dato: ', err);
